cb055d1f33e17035686cd9662294eb4090a82ee1
Ciibelle <cibellegon6565@gmail.com>
2022-05-24 21:40:44 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Atividade4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gc = 31.5;  
+            double fh = 0;
+            
+            fh = (9*gc+160)/5;
+            Console.WriteLine($"A conversão dessa temperatura para Fahrenheit é: {fh}");  
+        }
+    }
+}
